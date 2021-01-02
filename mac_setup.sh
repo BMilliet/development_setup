12d2ec873fa4395aae62f76749f5a07758162fde
BMilliet <39925357+BMilliet@users.noreply.github.com>
2021-01-02 11:51:16 -0300

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,28 +2,18 @@
 
 # install brew
 
-brew update
-brew upgrade
-
 brew install git
 brew cask install iterm2
 brew install zsh
+brew install vim
+brew install rbenv
 brew cask install slack
-brew cask install visual-studio-code
 brew cask install postman
 brew cask install firefox
 brew cask install brave-browser
 brew install python3
-brew install ruby
 
 brew cleanup
 brew list
 
-#install oh my zsh
-
-#cocoapods setup
-
-# export GEM_HOME=$HOME/.gem/ruby/2.3.0
-# export PATH=$GEM_HOME/bin:$PATH
-
-#gem install cocoapods --user-install
+# eval "$(rbenv init -)"
